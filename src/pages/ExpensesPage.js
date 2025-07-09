fd7e01f8c3ff8982b14504c72ad3410c9037695c
M-Saleh-N <salehnasser65784@gmail.com>
2025-07-09 11:17:13 +0300

    added new code

--- src/pages/ExpensesPage.js
+++ src/pages/ExpensesPage.js
@@ -1,13 +1,32 @@
 import ExpenseForm from '../components/expenses/ExpenseForm';
 import ExpenseList from '../components/expenses/ExpenseList';
 import ExpenseSummary from '../components/expenses/ExpenseSummary';
+import ExportCSV from '../components/common/ExportCSV';
+import { useExpenses } from '../contexts/ExpenseContext';
 
 const ExpensesPage = () => {
+  const { expenses } = useExpenses();
+
+  const expenseCSV = expenses.map(exp => ({
+    ID: exp.id,
+    Name: exp.name,
+    Category: exp.category,
+    Amount: exp.amount,
+    Date: new Date(exp.date).toLocaleDateString(),
+  }));
+
   return (
-    <div className="grid md:grid-cols-2 gap-6">
+    <div className="grid md:grid-cols-3 gap-6">
       <ExpenseForm />
       <ExpenseList />
-      <ExpenseSummary />
+      <div>
+        <ExpenseSummary />
+        <ExportCSV
+          data={expenseCSV}
+          filename="expenses.csv"
+          label="📥 Download Expenses CSV"
+        />
+      </div>
     </div>
   );
 };
