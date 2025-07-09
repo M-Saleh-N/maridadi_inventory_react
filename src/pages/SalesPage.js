3ad46d0f172e203512cfe444cd68c0a7d88afc0c
M-Saleh-N <salehnasser65784@gmail.com>
2025-07-09 10:16:01 +0300

    updated the salespage

--- src/pages/SalesPage.js
+++ src/pages/SalesPage.js
@@ -1,11 +1,15 @@
 import SalesForm from '../components/sales/SalesForm';
 import SalesHistory from '../components/sales/SalesHistory';
+import ExportSalesCSV from '../components/sales/ExportSalesCSV';
 
 const SalesPage = () => {
   return (
     <div className="grid md:grid-cols-2 gap-6">
       <SalesForm />
+      <div>
       <SalesHistory />
+       <ExportSalesCSV />
+       </div>
     </div>
   );
 };
