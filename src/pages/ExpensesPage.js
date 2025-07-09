a8e3eeca8bf3a29130d0c87d648cf5e3407f24f2
M-Saleh-N <salehnasser65784@gmail.com>
2025-07-09 10:05:23 +0300

    imported and linked expensessummary

--- src/pages/ExpensesPage.js
+++ src/pages/ExpensesPage.js
@@ -1,11 +1,13 @@
 import ExpenseForm from '../components/expenses/ExpenseForm';
 import ExpenseList from '../components/expenses/ExpenseList';
+import ExpenseSummary from '../components/expenses/ExpenseSummary';
 
 const ExpensesPage = () => {
   return (
     <div className="grid md:grid-cols-2 gap-6">
       <ExpenseForm />
       <ExpenseList />
+      <ExpenseSummary />
     </div>
   );
 };
